0239de1c1aaf27fb42343774a70d7b7ba09158be
i3visio <contacto@i3visio.com>
2015-01-17 02:54:41 +0100

    Added transform for opening URI in the system's default bbrowser

---
+++ osrframework/maltfy/uriToBrowser.py
@@ -0,0 +1,41 @@
+# !/usr/bin/python
+# -*- coding: cp1252 -*-
+#
+##################################################################################
+#
+#    This program is part of OSRFramework. You can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##################################################################################
+
+import webbrowser as wb
+
+from osrframework.maltfy.lib.maltego import *
+
+def uriToPort(uri=None):
+    ''' 
+        Method that launches the URI in the default browser of the system. This returns no new entity.
+
+        :param uri:    uri to open.
+    '''
+    me = MaltegoTransform()
+
+    wb.open(uri, new=2)    
+
+    me.returnOutput()
+
+
+if __name__ == "__main__":
+    uriToPort(uri=sys.argv[1])
+
+
