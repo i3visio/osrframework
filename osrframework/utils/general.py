d02601f9178a209fc4434bc78807b03fc607261f
i3visio <contacto@i3visio.com>
2015-07-10 20:27:16 +0200

    Using pyexcel to improve usufy's export capabilities: .ods, .txt, .xls and .xlsx.

--- osrframework/utils/general.py
+++ osrframework/utils/general.py
@@ -28,33 +28,58 @@ from osrframework.transforms.lib.maltego import MaltegoEntity, MaltegoTransform
 
 import logging
 
-def dictToJson(d):
+def exportUsufy(data, ext, fileH):
     '''
-        Workaround to convert any dictionary to json text.
-        
-        :param d:    Dictionary to convert to json.
-
-        :return:    jsonText (string).
+        Method that exports a Usufy structure onto different formats.
+        :param data: Data to export.
+        :param ext: One of the following: csv, excel, json, ods.
+        :param fileH: Fileheader for the output files.
     '''
-    # creating json
+    # Selecting the appropriate export function
+    if ext == "csv":
+        usufyToCsvExport(data, fileH+"."+ext)
+    elif ext == "json":
+        usufyToJsonExport(data, fileH+"."+ext)
+    elif ext == "mtz":
+        usufyToMaltegoExport(data, fileH+"."+ext)
+    elif ext == "ods":
+        usufyToOdsExport(data, fileH+"."+ext)
+    elif ext == "txt":
+        usufyToTextExport(data, fileH+"."+ext)        
+    elif ext == "xls":
+        usufyToXlsExport(data, fileH+"."+ext)
+    elif ext == "xlsx":
+        usufyToXlsxExport(data, fileH+"."+ext)
+                
+def usufyToJsonExport(d, fPath):
+    '''
+        Workaround to export to a json file.
+        :param d: Data to export.
+        :param fPath: File path.
+    '''
     jsonText =  json.dumps(d, indent=2, sort_keys=True)
-    return jsonText
+    with open (fPath, "w") as oF:
+        oF.write(jsonText)
 
-def dictToCSV(profiles):
+def _generateTabularData(res):
     '''
-        Workaround to convert any dictionary to CSV text.
-        
-        :param d:    Dictionary to convert to json.
-
-        :return:    csvText (string).
+        Method that recovers the values and columns from the current structure
+        This method is used by:
+            - usufyToCsvExport
+            - usufyToOdsExport
+            - usufyToXlsExport
+            - usufyToXlsxExport            
+        :param res:    the data to export.
+        :return:
+            values, a dictionary containing all the information stored.
+            headers, a list containing the headers used for the rows.
     '''
     # Dictionary of profiles found found
     values = {}
-    # List of columns
-    columns = ["i3visio.alias"]
+    headers = ["i3visio.alias"]
 
     # We are assuming that we received a list of profiles.
-    for p in profiles:    
+    for p in res:    
         alias = p["value"].split(' - ')[-1]
         # Creating the dictionaries
         values[p["value"]] = {}
@@ -65,18 +90,97 @@ def dictToCSV(profiles):
         for a in attributes:
             values[p["value"]][a["type"]] = a["value"]
             # Appending the column if not already included
-            if a["type"] not in columns:
-                columns.append(a["type"])
+            if a["type"] not in headers:
+                headers.append(a["type"])
+
+    # The information is stored as:
+    """
+        {
+            "Sheet 1" : [
+                ["value at row 1 col A", "value at row 1 col B"], 
+                ["value at row 2 col A", "value at row 2 col B"]
+            ],
+            "Sheet 2" : [
+                ["value at row 1 col A", "value at row 1 col B"], 
+                ["value at row 2 col A", "value at row 2 col B"],
+                ["value at row 3 col A", "value at row 3 col B"]                
+            ]            
+        }
+    """
+    data = {}    
+    # Note that each row should be a list!
+    workingSheet = []
+
+    # Appending the headers
+    workingSheet.append(headers)
+
+    for prof in values.keys():
+        # Creating an empty structure
+        newRow = []
+        for col in headers:
+            try:
+                newRow.append(values[prof][col])
+            except:
+                # Printing a Not Applicable value
+                newRow.append("N/A")
+        # Appending the newRow to the data structure
+        workingSheet.append(newRow)
+        
+    # Storing the workingSheet onto the data structure to be stored
+    data.update({"Usufy sheet": workingSheet})
+                
+    return data
+
+
+def usufyToCsvExport(data, fPath):
+    '''
+        Workaround to export to a CSV file.
+        :param data: Data to export.
+        :param fPath: File path.
+    '''
+    # Dictionary of profiles found found
+    values = {}
+    headers = ["i3visio.alias"]
+
+    # We are assuming that we received a list of profiles.
+    for p in data:    
+        alias = p["value"].split(' - ')[-1]
+        # Creating the dictionaries
+        values[p["value"]] = {}
+        values[p["value"]]["i3visio.alias"] = alias
+                
+        attributes = p["attributes"]
+        # Processing all the attributes found
+        for a in attributes:
+            values[p["value"]][a["type"]] = a["value"]
+            # Appending the column if not already included
+            if a["type"] not in headers:
+                headers.append(a["type"])
+
+    # We are assuming that we received a list of profiles.
+    for p in data:    
+        alias = p["value"].split(' - ')[-1]
+        # Creating the dictionaries
+        values[p["value"]] = {}
+        values[p["value"]]["i3visio.alias"] = alias
+                
+        attributes = p["attributes"]
+        # Processing all the attributes found
+        for a in attributes:
+            values[p["value"]][a["type"]] = a["value"]
+            # Appending the column if not already included
+            if a["type"] not in headers:
+                headers.append(a["type"])
     # Generating output   
     csvText = ""
     # Printing the headers
-    for col in columns:    
+    for col in headers:    
         csvText += col+"\t"
     csvText += "\n"
         
     for prof in values.keys():
         #csvText += alias + "\t"
-        for col in columns:
+        for col in headers:
             try:
                 csvText += values[prof][col] + "\t"
             except:
@@ -84,9 +188,99 @@ def dictToCSV(profiles):
                 #print col
                 csvText += "[N/A]\t"
         csvText += "\n"
-    return csvText
-    
+        
+    # Storing the file        
+    with open (fPath, "w") as oF:
+        oF.write(csvText)    
 
+def usufyToOdsExport(d, fPath):
+    '''
+        Workaround to export to a .ods file.
+        :param d: Data to export.
+        :param fPath: File path.
+    '''
+    tabularData = _generateTabularData(d)
+    from pyexcel_ods import save_data
+    # Storing the file        
+    save_data(fPath, tabularData)    
+
+def usufyToXlsExport(d, fPath):
+    '''
+        Workaround to export to a .xls file.
+        :param d: Data to export.
+        :param fPath: File path.
+    '''
+    tabularData = _generateTabularData(d)
+    from pyexcel_xls import save_data
+    # Storing the file        
+    save_data(fPath, tabularData)
+
+def usufyToXlsxExport(d, fPath):
+    '''
+        Workaround to export to a .xlsx file.
+        :param d: Data to export.
+        :param fPath: File path.
+    '''
+    tabularData = _generateTabularData(d)
+    from pyexcel_xlsx import save_data
+    # Storing the file        
+    save_data(fPath, tabularData)
+
+def usufyToTextExport(d, fPath=None):
+    '''
+        Workaround to export to a .txt file.
+        :param d: Data to export.
+        :param fPath: File path. If None was provided, it will assume that it has to print it.
+    '''
+    tabularData = _generateTabularData(d)["Usufy sheet"]
+    
+    import pyexcel as pe
+    import pyexcel.ext.text as text
+    
+    sheet = pe.Sheet(tabularData)
+    sheet.name = "Usufy results (" + getCurrentStrDatetime() +")."
+    # Defining the headers
+    sheet.name_columns_by_row(0)
+    text.TABLEFMT = "grid" 
+  
+    try:
+        with open(fPath, "w") as oF:
+            oF.write(str(sheet))
+    except:
+        # If a fPath was not provided... We will only print the info:
+        return sheet    
+
+def usufyToMaltegoExport(profiles, fPath):
+    '''
+        Workaround to export to a Maltego file.
+        :param d: Data to export.
+        :param fPath: File path.
+    '''
+    me = MaltegoTransform()
+    # A dictionary with the structure:
+
+    newEntities = []
+    for profile in profiles:
+	    # Defining the main entity
+        """aux ={}
+        aux["type"] = "i3visio.profile"
+        aux["value"] =  "Skype - " + str(user["i3visio.alias"])
+        aux["attributes"] = []    
+                
+        # Creation of a temp entity
+        aux = {}
+        aux["type"] = profile["value"]
+        aux["value"] = profile["type"]
+        aux["attributes"] = []"""
+        newEntities.append(profile)
+        
+    me.addListOfEntities(newEntities)        
+          
+    # Storing the file        
+    with open (fPath, "w") as oF:
+        oF.write(me.getOutput())    
+
+    
 def listToMaltego(profiles):
     """ 
         Method to generate the text to be appended to a Maltego file. May need to be revisited.
@@ -194,3 +388,61 @@ def getFilesFromAFolder(path):
     return onlyFiles
     
 
+################################################################################
+################################################################################
+################################################################################    
+'''
+
+    Kept for legacy reasons to avoid other applications calling these functions to crash
+    [TO-DO]
+        - Verify full compatibility if these functions are removed
+'''
+################################################################################
+################################################################################
+################################################################################
+def dictToJson(d):
+    '''
+        Workaround to convert any dictionary to json text.
+        
+        :param d:    Dictionary to convert to json.
+
+        :return:    jsonText (string).
+    '''
+    # creating json
+    jsonText =  json.dumps(d, indent=2, sort_keys=True)
+    return jsonText
+
+def dictToCSV(profiles):
+    '''
+        Workaround to convert any dictionary to CSV text.
+        
+        :param d:    Dictionary to convert to json.
+
+        :return:    jsonText (string).
+    '''
+    # We are assuming that we received a list of profiles.
+    values = {}
+    #values['i3visio.profile'] = []
+    values['i3visio.alias'] = []
+    values['i3visio.platform'] = []    
+    values['i3visio.uri'] = []        
+    
+    for p in profiles:
+        #values['i3visio.profile'].append(p['i3visio.profile'])
+        attributes = p["attributes"]
+
+        for a in attributes:
+            values[a["type"]].append(a["value"])
+    
+    csvText = ""
+    # Printing the headers
+    csvText += 'i3visio.alias'+"\t"
+    csvText += 'i3visio.platform'+"\t"
+    csvText += 'i3visio.uri'+"\n"
+    
+    # Printing the values
+    for i in range(len(values['i3visio.alias'])):
+        csvText += values['i3visio.alias'][i]+"\t"
+        csvText += values['i3visio.platform'][i]+"\t"
+        csvText += values['i3visio.uri'][i]+"\n"
+    return csvText
