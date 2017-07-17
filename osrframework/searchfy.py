35ccae5d3d4617fc166854cddf7a5c75a46c0689
i3visio <contacto@i3visio.com>
2017-07-17 12:41:47 +0200

    Fix several searchfy.py issues
    Add a timestamp to the searchfy command line util, as well as a
    better series of coloured notification messages and license.

--- osrframework/searchfy.py
+++ osrframework/searchfy.py
@@ -1,123 +1,116 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-##################################################################################
+################################################################################
 #
 #    Copyright 2015-2017 Félix Brezo and Yaiza Rubio (i3visio, contacto@i3visio.com)
 #
 #    This program is part of OSRFramework. You can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
+#    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##################################################################################
+################################################################################
 
-'''
-searchfy.py Copyright (C) F. Brezo and Y. Rubio (i3visio) 2015-2017
-This program comes with ABSOLUTELY NO WARRANTY.
-This is free software, and you are welcome to redistribute it under certain conditions. For additional info, visit to <http://www.gnu.org/licenses/gpl-3.0.txt>.
-'''
+
 __author__ = "Felix Brezo, Yaiza Rubio "
 __copyright__ = "Copyright 2015-2017, i3visio"
 __credits__ = ["Felix Brezo", "Yaiza Rubio"]
-__license__ = "GPLv3+"
-__version__ = "v5.0"
+__license__ = "AGPLv3+"
+__version__ = "v6.0"
 __maintainer__ = "Felix Brezo, Yaiza Rubio"
 __email__ = "contacto@i3visio.com"
 
 
 import argparse
+import datetime as dt
 import json
 import os
-from multiprocessing import Process, Pipe
 
 import osrframework.utils.banner as banner
 import osrframework.utils.platform_selection as platform_selection
 import osrframework.utils.configuration as configuration
 import osrframework.utils.general as general
 
-def getPlatformInfo(platform, query, process, mode, pipeConnection):
-    '''
-        Method to to get the info from the platform in a separete thread.
-        This is a fix to 'catch' segmentation faults from external platform libraries.
+from osrframework.utils.general import error, warning, success, info, title, emphasis
 
-        :param platform: <Platform> objects
-        :param queries: a query to be performed.
-        :param process: Whether to process all the profiles... SLOW!
-        :param mode: a string that represents a operation mode
-        :param pipeConnection: a pipe connection to send messages back to parent process
-
-        :return:
-    '''
-    # This returns a json.txt!
-    entities = platform.getInfo(query, process, mode)
-    pipeConnection.send(entities)
-    pipeConnection.close()
 
 def performSearch(platformNames=[], queries=[], process=False, excludePlatformNames=[]):
-    '''
-        Method to perform the phone list.
+    """
+    Method to perform the search itself.
 
-        :param platforms: List of <Platform> objects.
-        :param queries: List of queries to be performed.
-        :param process: Whether to process all the profiles... SLOW!
+    Args:
+    -----
+        platforms: List of <Platform> objects.
+        queries: List of queries to be performed.
+        process: Whether to process all the profiles... SLOW!
 
-        :return:
-    '''
+    Returns:
+    --------
+        A list with the entities collected.
+    """
     # Grabbing the <Platform> objects
     platforms = platform_selection.getPlatformsByName(platformNames, mode="searchfy", excludePlatformNames=excludePlatformNames)
     results = []
     for q in queries:
         for pla in platforms:
-            # Getting the pipe connection
-            parent_conn, child_conn = Pipe()
-            # Starting a new process
-            p = Process(target=getPlatformInfo, args=(pla, q, process, "searchfy", child_conn))
-            p.start()
-            p.join()
-            # Look at the exit code to check if the process ended unexpectedly
-            if p.exitcode == -11:
-                print "WARNING. Something happened when trying to get info from: " + str(pla)
-                print "\n\n"
-                continue
-            # Getting the entities from the message sent by child process
-            entitites = parent_conn.recv()
-            results += json.loads(entitites)
-            
+            # This returns a json.txt!
+            entities = pla.getInfo(query=q, process = process, mode="searchfy")
+            if entities != "[]":
+                results += json.loads(entities)
     return results
 
+
 def main(args):
-    '''
-        Main program.
+    """
+    Main program.
 
-        :param args: Arguments received in the command line.
-    '''
+    Args:
+    -----
+        args: Arguments received in the command line.
+
+    Returns:
+    --------
+        A list of i3visio entities.
+    """
     if not args.maltego:
-        print banner.text
-        sayingHello = """searchfy.py Copyright (C) F. Brezo and Y. Rubio (i3visio) 2016
-This program comes with ABSOLUTELY NO WARRANTY.
-This is free software, and you are welcome to redistribute it under certain conditions. For additional info, visit <http://www.gnu.org/licenses/gpl-3.0.txt>."""
-        print sayingHello
-        print
+        print(general.title(banner.text))
 
-    results = performSearch(platformNames=args.platforms, queries=args.queries, process=args.process, excludePlatformNames=args.exclude)
+        sayingHello = """
+searchfy.py Copyright (C) F. Brezo and Y. Rubio (i3visio) 2014-2017
+
+This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you
+are welcome to redistribute it under certain conditions. For additional info,
+visit """ + general.LICENSE_URL + "\n"
+        print(general.title(sayingHello))
+
+        # Showing the execution time...
+        if not args.maltego:
+            startTime= dt.datetime.now()
+            print(str(startTime) + "\tStarting search in " + general.emphasis(str(len(args.platforms))) + " platform(s)... Relax!\n")
+            print(general.emphasis("\tPress <Ctrl + C> to stop...\n"))
+
+    # Performing the search
+    try:
+        results = performSearch(platformNames=args.platforms, queries=args.queries, process=args.process, excludePlatformNames=args.exclude)
+    except KeyboardInterrupt:
+        print(general.error("\n[!] Process manually stopped by the user. Workers terminated without providing any result.\n"))
+        results = []
 
     # Generating summary files for each ...
     if args.extension:
         # Storing the file...
-        #logger.info("Creating output files as requested.")
         if not args.maltego:
             # Verifying if the outputPath exists
             if not os.path.exists (args.output_folder):
-                #logger.warning("The output folder \'" + args.output_folder + "\' does not exist. The system will try to create it.")
                 os.makedirs(args.output_folder)
 
         # Grabbing the results
@@ -135,24 +128,27 @@ This is free software, and you are welcome to redistribute it under certain cond
 
     # Printing the results if requested
     if not args.maltego:
-        print "A summary of the results obtained are listed in the following table:"
-        print unicode(general.usufyToTextExport(results))
+        now = dt.datetime.now()
+        print(str(now) + "\tA summary of the results obtained are listed in the following table:\n")
+        print(general.success(unicode(general.usufyToTextExport(results))))
 
         if args.web_browser:
             general.openResultsInBrowser(results)
 
-        print "You will find all the information collected in the following files:"
+        now = dt.datetime.now()
+        print("\n" + str(now) + "\tYou can find all the information collected in the following files:")
         for ext in args.extension:
-            # Generating output files
-            print "\t-" + fileHeader + "." + ext
+            # Showing the output files
+            print("\t" + general.emphasis(fileHeader + "." + ext))
 
-    # Urging users to place an issue on Github...
-    if not args.maltego:
-        print
-        print "Did something go wrong? Is a platform reporting false positives? Do you need to integrate a new one?"
-        print "Then, place an issue in the Github project: <https://github.com/i3visio/osrframework/issues>."
-        print "Note that otherwise, we won't know about it!"
-        print
+        # Showing the execution time...
+        endTime= dt.datetime.now()
+        print("\n" + str(endTime) +"\tFinishing execution...\n")
+        print("Total time used:\t" + general.emphasis(str(endTime-startTime)))
+        print("Average seconds/query:\t" + general.emphasis(str((endTime-startTime).total_seconds()/len(args.platforms))) +" seconds\n")
+
+        # Urging users to place an issue on Github...
+        print(banner.footer)
 
     return results
 
