734ccd52aae033929a5aeb230ef78d7a7ac00880
i3visio <contacto@i3visio.com>
2017-11-04 20:07:16 +0100

    Release OSRFramework 0.17.4

--- osrframework/__init__.py
+++ osrframework/__init__.py
@@ -23,4 +23,4 @@ import osrframework.utils.logger
 
 # Calling the logger when being imported
 osrframework.utils.logger.setupLogger(loggerName="osrframework")
-__version__="0.17.4b"
+__version__="0.17.4"
