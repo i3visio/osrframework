520b87bddfb81afcfd2c666b88b5dbb824fe21d5
i3visio <contacto@i3visio.com>
2018-05-21 00:33:42 +0200

    Add Instagram to searchfy
    To do so, we are using picbear.online platform to grab the results.

--- osrframework/wrappers/instagram.py
+++ osrframework/wrappers/instagram.py
@@ -30,12 +30,12 @@ import osrframework.utils.browser as browser
 from osrframework.utils.platforms import Platform
 
 class Instagram(Platform):
-    """ 
+    """
         A <Platform> object for Instagram.
     """
     def __init__(self):
-        """ 
-            Constructor... 
+        """
+            Constructor...
         """
         self.platformName = "Instagram"
         self.tags = ["image", "social"]
@@ -43,28 +43,28 @@ class Instagram(Platform):
         ########################
         # Defining valid modes #
         ########################
-        self.isValidMode = {}        
+        self.isValidMode = {}
         self.isValidMode["phonefy"] = False
         self.isValidMode["usufy"] = True
-        self.isValidMode["searchfy"] = False      
-        
+        self.isValidMode["searchfy"] = True
+
         ######################################
         # Search URL for the different modes #
         ######################################
         # Strings with the URL for each and every mode
-        self.url = {}        
+        self.url = {}
         #self.url["phonefy"] = "http://anyurl.com//phone/" + "<phonefy>"
-        self.url["usufy"] = "http://www.instagram.com/" + "<usufy>"       
-        #self.url["searchfy"] = "http://anyurl.com/search/" + "<searchfy>"       
+        self.url["usufy"] = "http://www.instagram.com/" + "<usufy>"
+        self.url["searchfy"] = "http://picbear.online/search/" + "<searchfy>"
 
         ######################################
         # Whether the user needs credentials #
         ######################################
-        self.needsCredentials = {}        
+        self.needsCredentials = {}
         #self.needsCredentials["phonefy"] = False
         self.needsCredentials["usufy"] = False
-        #self.needsCredentials["searchfy"] = False 
-        
+        self.needsCredentials["searchfy"] = False
+
         #################
         # Valid queries #
         #################
@@ -73,8 +73,8 @@ class Instagram(Platform):
         # The regular expression '.+' will match any query.
         #self.validQuery["phonefy"] = ".*"
         self.validQuery["usufy"] = ".+"
-        #self.validQuery["searchfy"] = ".*"
-        
+        self.validQuery["searchfy"] = ".*"
+
         ###################
         # Not_found clues #
         ###################
@@ -82,31 +82,30 @@ class Instagram(Platform):
         self.notFoundText = {}
         #self.notFoundText["phonefy"] = []
         self.notFoundText["usufy"] = ["Página no encontrada"]
-        #self.notFoundText["searchfy"] = []        
-        
+        self.notFoundText["searchfy"] = []
+
         #########################
         # Fields to be searched #
         #########################
         self.fieldsRegExp = {}
-        
+
         # Definition of regular expressions to be searched in phonefy mode
         #self.fieldsRegExp["phonefy"] = {}
         # Example of fields:
         #self.fieldsRegExp["phonefy"]["i3visio.location"] = ""
-        
+
         # Definition of regular expressions to be searched in usufy mode
         self.fieldsRegExp["usufy"] = {}
         # Example of fields:
         #self.fieldsRegExp["usufy"]["i3visio.location"] = ""
         # Definition of regular expressions to be searched in searchfy mode
-        #self.fieldsRegExp["searchfy"] = {}
-        # Example of fields:
-        #self.fieldsRegExp["searchfy"]["i3visio.location"] = ""        
-        
+        self.fieldsRegExp["searchfy"] = {}
+        #"<img alt=\"@([^\"]+)\""
+        self.searchfyAliasRegexp = "\" alt=\"@([^\"]+)\"> <\/div> <div class="
+        # Example of fields:"<img alt=\"@([^\"]+)\""
+
         ################
         # Fields found #
         ################
         # This attribute will be feeded when running the program.
         self.foundFields = {}
-        
-        
