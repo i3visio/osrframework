782a47d122655c8f2b006ab64aa020686cba51f6
febrezo <felixbrezo@disroot.org>
2020-04-29 03:08:08 +0200

    Fix #351: Update phonefy wrapper for Listaspam

--- osrframework/wrappers/listaspam.py
+++ osrframework/wrappers/listaspam.py
@@ -18,7 +18,7 @@
 ################################################################################
 
 __author__ = "Felix Brezo, Yaiza Rubio <contacto@i3visio.com>"
-__version__ = "2.0"
+__version__ = "3.0"
 
 
 from osrframework.utils.platforms import Platform
@@ -30,73 +30,16 @@ class Listaspam(Platform):
         self.platformName = "Listaspam"
         self.tags = ["phone"]
 
-        ########################
-        # Defining valid modes #
-        ########################
-        self.isValidMode = {}
-        self.isValidMode["phonefy"] = True
-        self.isValidMode["usufy"] = False
-        self.isValidMode["searchfy"] = False
-
-        ######################################
-        # Search URL for the different modes #
-        ######################################
-        self.url = {}
-        self.url["phonefy"] = "http://www.listaspam.com/busca.php?Telefono=<phonefy>"
-        #self.url["usufy"] = "http://anyurl.com/user/" + "<phonefy>"
-        #self.url["searchfy"] = "http://anyurl.com/search/" + "<phonefy>"
-
-        ######################################
-        # Whether the user needs credentials #
-        ######################################
-        self.needsCredentials = {}
-        self.needsCredentials["phonefy"] = False
-        self.needsCredentials["usufy"] = False
-        self.needsCredentials["searchfy"] = False
-
-        #################
-        # Valid queries #
-        #################
-        # Strings that will imply that the query number is not appearing
-        self.validQuery = {}
-        # The regular expression '.+' will match any query.
-        self.validQuery["phonefy"] = ".+"
-        self.validQuery["usufy"] = ".+"
-        self.validQuery["searchfy"] = ".+"
-
-        ###################
-        # Not_found clues #
-        ###################
-        # Strings that will imply that the phone number is not appearing
-        self.notFoundText = {}
-        self.notFoundText["phonefy"] = [
-            "No te quedes sin saber quien te llama por teléfono."
-        ]
-        #self.notFoundText["usufy"] = []
-        #self.notFoundText["searchfy"] = []
-
-        #########################
-        # Fields to be searched #
-        #########################
-        self.fieldsRegExp = {}
-        # Definition of regular expressions to be searched in phonefy mode
-        self.fieldsRegExp["phonefy"] = {}
-        self.fieldsRegExp["phonefy"]["i3visio.location.province"] = "<strong class='located_label'>(.*),"
-        self.fieldsRegExp["phonefy"]["i3visio.location.country"] = "class='country_located' alt='([a-zA-Zñ]*)'"
-        self.fieldsRegExp["phonefy"]["i3visio.text"] = '<h4 class="media-heading">(.*)</p>'
-
-        # Definition of regular expressions to be searched in usufy mode
-        #self.fieldsRegExp["usufy"] = {}
-        # Example of fields:
-        #self.fieldsRegExp["usufy"]["i3visio.location"] = ""
-
-        # Definition of regular expressions to be searched in searchfy mode
-        #self.fieldsRegExp["searchfy"] = {}
-        # Example of fields:
-        #self.fieldsRegExp["searchfy"]["i3visio.location"] = ""
-
-        ################
-        # Fields found #
-        ################
-        # This attribute will be feeded when running the program.
-        self.foundFields = {}
+        self.modes = {
+            "phonefy": {
+                "debug": False,
+                "extra_fields": {
+                    "com.i3visio.Location": "<div>📍([^<]+)</div>",
+                    "com.i3visio.Labels": 'Nube de tags: (.+)</div>'
+                },
+                "needs_credentials": False,
+                "not_found_text": "No te quedes sin saber quién te llama por teléfono.",                   # Text that indicates a missing profile
+                "query_validator": "[0-9+\-\.]{6-16}",                            # Regular expression that the alias SHOULD match
+                "url": "http://www.listaspam.com/busca.php?Telefono={placeholder}",       # Target URL where {placeholder} would be modified by the alias
+            }
+        }
